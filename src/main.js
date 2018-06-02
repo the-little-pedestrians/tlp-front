b62f189da5f5d05650ccd857f5979b37586a0608
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-06-02 15:22:56 +0200

    Added vuetify

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuetify from 'vuetify'
+
 import App from './App'
 import router from './router'
 
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#E53935',
+    secondary: '#FFCDD2',
+    accent: '#3F51B5'
+  }
+})
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
